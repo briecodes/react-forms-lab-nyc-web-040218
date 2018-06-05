e3b25a94c4e2f9a9b09a96657e7a466a85fec137
briecodes <hello@brie.work>
2018-06-05 18:46:06 -0400

    progress.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,31 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  switcheroo = (e) => {
+    this.props.onSubmit();
+    if(this.state.username.length > 0 && this.state.password.length > 0){
+    }
+  }
+
+  update = (e) => {
+    if (e.target.id === 'test-username'){
+      this.setState({
+        username: e.target.value
+      })
+    }else{
+      this.setState({
+        password: e.target.value
+      })
+    }
   }
 
   render() {
@@ -13,17 +34,17 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="test-username" type="text" />
+            <input id="test-username" type="text" value={this.state.username} onChange={this.update} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" />
+            <input id="test-password" type="password" value={this.state.password} onChange={this.update} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onClick={this.switcheroo}>Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      message: '',
+      remainingChars: 0
+    };
+  }
+
+  validate = (e) => {
+    this.setState({
+      message: e.target.value
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input onChange={this.validate} type="text" value={this.state.message} /><div id="amount">{this.props.maxChars - this.state.message.length} characters left</div>
       </div>
     );
   }

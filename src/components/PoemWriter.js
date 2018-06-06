3ef31db37713fb7cfe3dee5c8e6ec8446d69bd0c
briecodes <hello@brie.work>
2018-06-06 11:53:38 -0400

    lab completed.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -13,8 +13,7 @@ class PoemWriter extends React.Component {
   updateState = (value) => {
     this.setState({
       poem: value,
-      valid: this.isValid(),
-      warning: ""
+      valid: this.isValid(value)
     })
   }
 
@@ -22,30 +21,26 @@ class PoemWriter extends React.Component {
     this.updateState(e.target.value);
   }
 
-  isValid = () => {
-    if (this.lineArray().length === 3 && this.wordCount(0) === 5 && this.wordCount(1) === 3 && this.wordCount(2) === 5 && this.state.poem !== '') {
+  isValid = (poem) => {
+    if (this.lineArray(poem).length === 3 && this.lineArray(poem)[0].trim().split(" ").length === 5 && this.lineArray(poem)[1].trim().split(" ").length === 3 && this.lineArray(poem)[2].trim().split(" ").length === 5 && poem !== "") {
       return true
     } else {
       return false
     }
   }
 
-  lineArray = () => {
-    var text = this.state.poem.replace(/\s+$/g, "")
+  lineArray = (poem) => {
+    // var text = poem.replace(/\s+$/g, "")
+    var text = poem.replace(/  +/g, ' ');
     var split = text.split("\n")
     return split
-  }
-
-  wordCount = (line) => {
-    let wordArr = this.lineArray()[line].split(" ");
-    return wordArr.length;
   }
 
   render() {
     return (
       <div>
         <textarea rows="3" cols="60" onChange={this.poemCheck} value={this.state.poem} />
-        { !this.state.valid ? (<div id="poem-validation-error" style={{ color: "red" }}>This poem is not written in the right structure!</div>) : null }
+        { this.state.valid ? null : (<div id="poem-validation-error" style={{ color: "red" }}>This poem is not written in the right structure!</div>) }
       </div>
     );
   }

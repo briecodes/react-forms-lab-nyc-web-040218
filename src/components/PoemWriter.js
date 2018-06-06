0e0fdc224978090cf3c939c61cd7b9c6e379bc49
briecodes <hello@brie.work>
2018-06-05 23:01:31 -0400

    stuck at validation.

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -5,26 +5,28 @@ class PoemWriter extends React.Component {
     super();
 
     this.state = {
-      poem: ''
+      poem: '',
+      valid: true
     };
   }
 
   updateState = (value) => {
     this.setState({
       poem: value,
+      valid: this.isValid(),
       warning: ""
     })
   }
 
   poemCheck = (e) => {
     this.updateState(e.target.value);
+  }
 
-    if (this.lineArray().length === 3 && this.wordCount(0) === 5 && this.wordCount(1) === 3 && this.wordCount(2) === 5 && this.state.poem !== ''){
-      console.log("this is valid.");
-    }else{
-      this.setState({
-        warning: "This poem is not written in the right structure!"
-      })
+  isValid = () => {
+    if (this.lineArray().length === 3 && this.wordCount(0) === 5 && this.wordCount(1) === 3 && this.wordCount(2) === 5 && this.state.poem !== '') {
+      return true
+    } else {
+      return false
     }
   }
 
@@ -43,9 +45,7 @@ class PoemWriter extends React.Component {
     return (
       <div>
         <textarea rows="3" cols="60" onChange={this.poemCheck} value={this.state.poem} />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          {this.state.warning}
-        </div>
+        { !this.state.valid ? (<div id="poem-validation-error" style={{ color: "red" }}>This poem is not written in the right structure!</div>) : null }
       </div>
     );
   }
